3e9c489f542af5fb72873bdecfda794fe5b6fbd8
blubtxt <admin@devwerks.net>
2014-01-21 09:29:19 +0100

    fixed Connections

--- honey-stats.py
+++ honey-stats.py
@@ -29,40 +29,17 @@ dbfileglastopf = '/opt/myhoneypot/db/glastopf.db'
 def generateGraphs():
 	version()
         
-        conn = sqlite3.connect(dbfiledionaea)
-	c = conn.cursor()
+        conndio = sqlite3.connect(dbfiledionaea)
+	c = conndio.cursor()
+
+        connglas = sqlite3.connect(dbfileglastopf)
+        x = connglas.cursor()
         
-        #Connections per Day - 7 Days
-        querySQL = "SELECT strftime('%Y', connection_timestamp,'unixepoch') as 'year', strftime('%m', connection_timestamp,'unixepoch') as 'month', strftime('%d', connection_timestamp,'unixepoch') as 'day', count(strftime('%m', connection_timestamp,'unixepoch')) as 'num' FROM connections GROUP BY strftime('%Y', connection_timestamp,'unixepoch'), strftime('%m', connection_timestamp,'unixepoch'), strftime('%d', connection_timestamp,'unixepoch') ORDER BY strftime('%Y', connection_timestamp,'unixepoch') DESC, strftime('%m', connection_timestamp,'unixepoch') DESC, strftime('%d', connection_timestamp,'unixepoch') DESC LIMIT 7"
-        print querySQL
-        c.execute(querySQL)
-        
-        chart = SimpleLineChart(pieChartWidth, pieChartHeight, y_range=[0, 5000])
-        
-        list = []
-        seclist = []
-        
-	for row in c:
-		print(row)
-                list.append(row[3])
-                date = '%s-%s-%s' % (str(row[0]),str(row[1]),str(row[2]))
-                seclist.append(date)
-                
-        list.reverse()
-        seclist.reverse()
-                
-        chart.add_data(list)
-        chart.set_axis_labels(Axis.BOTTOM,seclist)
-        
-        chart.set_colours(['0000FF'])
-        chart.fill_linear_stripes(Chart.CHART, 0, 'CCCCCC', 0.2, 'FFFFFF', 0.2)
-        chart.set_grid(0, 20, 5, 5)
-        
-        left_axis = range(0, 5001, 1000)
-        left_axis[0] = ''
-        chart.set_axis_labels(Axis.LEFT, left_axis)
-         
-        chart.download('connections_per_day.png')
+        #
+        #
+        # Dionaea Queries starting here!
+        #
+        #
 
         #Most attacked ports
         querySQL = 'SELECT COUNT(local_port) AS hitcount,local_port AS port FROM connections WHERE connection_type = "accept" GROUP BY local_port HAVING COUNT(local_port) > 10'
@@ -71,15 +48,15 @@ def generateGraphs():
         
         chart = PieChart2D(pieChartWidth, pieChartHeight, colours=pieColours)
         
-        list = []
+        flist = []
         seclist = []
         
 	for row in c:
 		print(row)
-                list.append(row[0])
+                flist.append(row[0])
                 seclist.append(str(row[1]))
                 
-        chart.add_data(list)
+        chart.add_data(flist)
         chart.set_legend(seclist)
 
         chart.download('attacked_ports.png')
@@ -91,44 +68,81 @@ def generateGraphs():
 			
         chart = PieChart2D(pieChartWidth, pieChartHeight, colours=pieColours)
         
-        list = []
+        flist = []
         seclist = []
         
 	for row in c:
 		print(row)
-                list.append(row[0])
+                flist.append(row[0])
                 seclist.append(str(row[1]))
                 
-        chart.add_data(list)
+        chart.add_data(flist)
         chart.set_legend(seclist)
 
         chart.download('popular_malware.png')
+        
+        #Connections per Day Dionaea and Glastopf - 7 Days - Dionaea
+        querySQL = "SELECT strftime('%Y', connection_timestamp,'unixepoch') as 'year', strftime('%m', connection_timestamp,'unixepoch') as 'month', strftime('%d', connection_timestamp,'unixepoch') as 'day', count(strftime('%m', connection_timestamp,'unixepoch')) as 'num' FROM connections GROUP BY strftime('%Y', connection_timestamp,'unixepoch'), strftime('%m', connection_timestamp,'unixepoch'), strftime('%d', connection_timestamp,'unixepoch') ORDER BY strftime('%Y', connection_timestamp,'unixepoch') DESC, strftime('%m', connection_timestamp,'unixepoch') DESC, strftime('%d', connection_timestamp,'unixepoch') DESC LIMIT 7"
+        print querySQL
+        c.execute(querySQL)
+        
+        #Connections per Day Dionaea and Glastopf - 7 Days - Glastopf
+        querySQL = "SELECT COUNT(time), SUBSTR(time,-20,12) AS stripped FROM events GROUP BY stripped ORDER BY stripped DESC LIMIT 7"
+        print querySQL
+        x.execute(querySQL)
+        
+        chart = SimpleLineChart(pieChartWidth, pieChartHeight, y_range=[0, 5000])
+        
+        flist = []
+        seclist = []
+        
+	for (row,rowx) in zip(c,x):
+                 print(row)
+                 print(rowx)
+                 l = list(row)
+                 l[3]=row[3]+rowx[0]
+                 flist.append(l[3])
+                 date = '%s-%s-%s' % (str(row[0]),str(row[1]),str(row[2]))
+                 seclist.append(date)
+                
+        flist.reverse()
+        seclist.reverse()
+                
+        chart.add_data(flist)
+        chart.set_axis_labels(Axis.BOTTOM,seclist)
+        
+        chart.set_colours(['0000FF'])
+        chart.fill_linear_stripes(Chart.CHART, 0, 'CCCCCC', 0.2, 'FFFFFF', 0.2)
+        chart.set_grid(0, 20, 5, 5)
+        
+        left_axis = range(0, 5001, 1000)
+        left_axis[0] = ''
+        chart.set_axis_labels(Axis.LEFT, left_axis)
+         
+        chart.download('connections_per_day.png')
         
         #
         #
         # Glastopf Queries starting here!
         #
         #
-  
-        conn = sqlite3.connect(dbfileglastopf)
-        c = conn.cursor()
         
         #Top15 intext requests
         querySQL = 'SELECT count, content FROM intext ORDER BY count DESC LIMIT 15'
         print querySQL
-        c.execute(querySQL)
+        x.execute(querySQL)
         
         chart = PieChart2D(pieChartWidth, pieChartHeight, colours=pieColours)
         
-        list = []
+        flist = []
         seclist = []
         
-        for row in c:
+        for row in x:
 		print(row)
-                list.append(row[0])
+                flist.append(row[0])
                 seclist.append(str(row[1]))
                 
-        chart.add_data(list)
+        chart.add_data(flist)
         chart.set_legend(seclist)
 
         chart.download('popular_intext.png')
@@ -136,19 +150,19 @@ def generateGraphs():
         #Top15 intitle requests
         querySQL = 'SELECT count, content FROM intitle ORDER BY count DESC LIMIT 15'
         print querySQL
-        c.execute(querySQL)
+        x.execute(querySQL)
         
         chart = PieChart2D(pieChartWidth, pieChartHeight, colours=pieColours)
         
-        list = []
+        flist = []
         seclist = []
         
-        for row in c:
+        for row in x:
 		print(row)
-                list.append(row[0])
+                flist.append(row[0])
                 seclist.append(str(row[1]))
                 
-        chart.add_data(list)
+        chart.add_data(flist)
         chart.set_legend(seclist)
 
         chart.download('popular_intitle.png')
@@ -156,25 +170,25 @@ def generateGraphs():
         #Top10 inurl requests
         querySQL = 'SELECT count, content FROM inurl ORDER BY count DESC LIMIT 10'
         print querySQL
-        c.execute(querySQL)
+        x.execute(querySQL)
         
         chart = PieChart2D(pieChartWidth, pieChartHeight, colours=pieColours)
         
-        list = []
+        flist = []
         seclist = []
         
-        for row in c:
+        for row in x:
 		print(row)
-                list.append(row[0])
+                flist.append(row[0])
                 seclist.append(str(row[1]))
                 
-        chart.add_data(list)
+        chart.add_data(flist)
         chart.set_legend(seclist)
 
         chart.download('popular_inurl.png')
 
 def version():
-	sys.stdout.write("\nHoneypot Statistics 0.1\n")
+	sys.stdout.write("\nHoneypot Statistics 0.2\n")
 	sys.stdout.write("Author: Johannes Schroeter - www.devwerks.net\n\n")
 
 def main():
